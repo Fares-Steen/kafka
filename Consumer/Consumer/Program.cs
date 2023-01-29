2f9fb5b27208d5dd6e924705dc67cf06dc9a4dce
fares <fares.steen@gmail.com>
2023-01-29 11:47:03 +0100

    finish one consumer

--- Consumer/Consumer/Program.cs
+++ Consumer/Consumer/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
 
-Console.WriteLine("Hello, World!");
+var config = new ConsumerConfig
+{
+    GroupId = "weather-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+consumer.Subscribe("weather-topic");
+CancellationToken token = new();
+
+try
+{
+    while (true)
+    {
+        var response = consumer.Consume(token);
+        if (response.Message != null)
+        {
+            var weather = JsonConvert.DeserializeObject<Weather>(response.Message.Value);
+            Console.WriteLine($"{weather!.State} temperature {weather.Temperature}");
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    throw;
+}
+public record Weather(string State, int Temperature); 
--- Producer/Producer/Program.cs
+++ Producer/Producer/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
-Console.WriteLine("Hello, World!");
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092",
+};
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+try
+{
+    string? state;
+    while ((state = Console.ReadLine() )!=null)
+    {
+        var response = await producer.ProduceAsync("weather-topic",
+            new Message<Null, string> { Value = JsonConvert.SerializeObject(new Weather(state,90)) });
+
+        Console.WriteLine(response.Message.Value);
+    }
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+public record Weather(string State, int Temperature); 
